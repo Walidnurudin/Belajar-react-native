19751cb98f061f757cbebb712c0f9f3a84e7ef64
Walid nurudin <walidnurudin47@gmail.com>
2020-10-06 21:48:20 +0700

    belajar RN

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View, TextInput} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{backgroundColor: 'red'}}>Walid</Text>
+      <TextInput style={{borderWidth: 1}} />
+    </View>
+  )
+}
+
+export default App;
